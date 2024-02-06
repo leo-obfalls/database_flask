ff482bb61e859b22e46c981063721d6ccbfcd9cf
leo-obfalls <98482308+leo-obfalls@users.noreply.github.com>
2024-02-06 13:42:35 +0000

    connected to database

--- app.py
+++ app.py
@@ -1,33 +1,30 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template
-import os
 import psycopg2
+from config import db_config
+
+
 app = Flask(__name__)
 
-DB_HOST = os.environ['DB_HOST']
-DB_USER = os.environ['DB_USER']
-DB_PASSWORD = os.environ['DB_PASSWORD']
-DB_NAME = os.environ['DB_NAME']
+dbname = db_config['dbname']
+user = db_config['user']
+password = db_config['password']
+host = db_config['host']
+port = db_config['port']
 
-
-conn = psycopg2.connect(
-    dbname=DB_NAME,
-    user=DB_USER,
-    password=DB_PASSWORD,
-    host=DB_HOST,
-    port="5432"
-)
+def get_db_connection():
+        conn = psycopg2.connect(**db_config)
+        return conn
 
 @app.route("/")
 def hello():
     return render_template('home.html')
 
+
+
 @app.route('/index')
 def index():
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM manuscript")
-    data = cursor.fetchall()
-    cursor.close()
-
+    conn = get_db_connection()
     return render_template('index.html', data=data)
 
 if __name__ == "__main__":
